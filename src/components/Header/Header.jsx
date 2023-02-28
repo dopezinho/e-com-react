7af4189418405778cc0524dee1e3eece21db9987
dopezinho <opugnero@gmail.com>
2023-02-28 16:05:26 -0300

    clean

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,6 @@ const Header = () => {
   return (
     <div className="header">
       <img className="header__img" src={mangoIcon} alt="mango icon" />
-      <p className="header__paragraph">num é</p>
-      <p className="header__paragraph header__paragraph--red">é</p>
     </div>
   )
 }
