de85a7bcccd0b814508fc56cf5eeb474c5683a84
dopezinho <opugnero@gmail.com>
2023-02-28 11:55:48 -0300

    sass ready

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,13 +4,17 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
+
     <div className="home">
       <div className="container">
         <h1>Home</h1>
-        <button>
-            <Link to={`/products/1`}>Produto</Link>
-        </button>
       </div>
+
+      <button>
+        <Link to={`/products/1`}>Produto</Link>
+      </button>
+
+      <button>Testando main butt</button>
     </div>
   )
 }
