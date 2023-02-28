93a965c84ff6ce9309f1832ea55ca8a766d43138
dopezinho <opugnero@gmail.com>
2023-02-28 11:06:35 -0300

    start

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {  createBrowserRouter,  RouterProvider,} from "react-router-dom";
+import Login from './views/Login/Login';
+import Home from './views/Home/Home';
+import Products from './views/Products/Products';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/products/:productId",
+    element: <Products />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
