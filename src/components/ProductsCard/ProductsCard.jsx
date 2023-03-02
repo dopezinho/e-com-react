c0ab6cb24373939ee162cf03985fef91302deccf
dopezinho <opugnero@gmail.com>
2023-03-02 11:56:26 -0300

    mangos align 0

--- src/components/ProductsCard/ProductsCard.jsx
+++ src/components/ProductsCard/ProductsCard.jsx
@@ -3,13 +3,15 @@ import "./index.scss";
 
 const ProductsCard = ({data}) => {
   return (
-    <div className="home__card">
-      <img src={data.imgPath} alt="model" />
-      <div className="home__card-description">
-        <p>{data.title}</p>
-        <button>
-          <Link to={`/products/${data.id}`}>Comprar Look</Link>
-        </button>
+    <div className="homere">
+      <div className="home__card">
+        <img src={data.imgPath} alt="model" />
+        <div className="home__card-description">
+          <p>{data.title}</p>
+          <button>
+            <Link to={`/products/${data.id}`}>Comprar Manga</Link>
+          </button>
+        </div>
       </div>
     </div>
   );;
