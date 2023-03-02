051f682650054e04b5aa6d555a7a973886f6346e
dopezinho <opugnero@gmail.com>
2023-03-02 12:34:01 -0300

    product detail jsx

--- src/views/Products/Products.jsx
+++ src/views/Products/Products.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { useParams } from "react-router-dom"
+import HeaderMenu from '../../components/HeaderMenu/HeaderMenu'
+import ProductDetail from "../../components/ProductDetail/ProductDetail"
+import Search from "../../components/Search/Search"
 import './index.scss'
 
 const Products = () => {
+  const params = useParams()
+
   return (
-    <div>Products</div>
-  )
+    <section className="product">
+      <HeaderMenu />
+      <Search />
+      <ProductDetail //data={selectProduct}
+       />
+    </section>
+  ) 
 }
 
 export default Products
